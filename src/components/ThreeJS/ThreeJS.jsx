54b04f2784339d83eb84cb3c1ce8e8e4d3d560d9
Sutsiki <nicolas-dutertre@bbox.fr>
2022-08-02 20:20:53 +0200

    Add ThreeJS

---
+++ src/components/ThreeJS/ThreeJS.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './threejs.scss'
+
+export const ThreeJS = () => {
+  return (
+    <h1 className='threejs'> THREE JS ! </h1>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from '../../components/NavBar/Navbar'
+import { ThreeJS } from '../../components/ThreeJS/ThreeJS'
 import './home.scss'
 
 export const Home = () => {
   return (
     <div className="home">
         <Navbar/>
+        <ThreeJS/>
     </div>
   )
 }
